66b2a20cbc445850d9caf28ef2f504ba977c7a98
JackWang <jack3shihong@gmail.com>
2022-04-26 22:28:13 +0800

    changed print to logger

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
 Notes:
     心得:
-        1. 只有必要的时候在用Tensor, 因为Tensor具有 grad, view, device等复杂属性
+        1. 只有必要的时候在用Tensor, 因为Tensor具有 grad, view, device等复杂属性, 一般的视图也会被记录grad, 留下reference,
+            导致不必要的内存开销, 所以如无必要勿用Tensor, Numpy解决就行了
         2. 如果必须要用Tensor, 或者需要使用非训练中的网络来进行计算, 一定要使用torch.no_grad
         3. 数据集中尽量使用np.ndarray, 然后可以用上torchvision.transforms
+        4. 写完了之后, 多用用logging, 便于保留每一次训练结果
 """
 
 # Torch Library
@@ -14,6 +16,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 # Standard Library
 import copy
+import logging
 import datetime
 from typing import *
 from pathlib import Path
@@ -39,7 +42,7 @@ class CLTrainer:
 
     default_dtype: torch.dtype = torch.float32
 
-    def __init__(self, network: iCaLRNet, dry_run: bool=True) -> None:
+    def __init__(self, network: iCaLRNet, dry_run: bool=True, log: bool = False) -> None:
         self.dry_run: bool = dry_run
 
 
@@ -63,13 +66,45 @@ class CLTrainer:
         if not dry_run:
             self.checkpoint_path.mkdir(parents=True, exist_ok=True)
         
+        # log
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.INFO)
+        fmt = logging.Formatter(fmt="%(message)s", datefmt="")
 
-    
+        terminal = logging.StreamHandler()
+        terminal.setLevel(logging.INFO)
+        terminal.setFormatter(fmt=fmt)
+        self.logger.addHandler(terminal)
+
+        self.log = log
+        if log:
+            self.log_path = legal_converter(ProjectPath.logs / suffix / self.start_time / "trian.log")
+            self.log_path.parent.mkdir(parents=True, exist_ok=True)
+            self.logger.info(f"{Fore.GREEN}Save log to {self.log_path}")
+            file = logging.FileHandler(filename=str(self.log_path))
+            file.setLevel(logging.INFO)
+            file.setFormatter(fmt=fmt)
+            self.logger.addHandler(file)
+
+
     def __del__(self):
         if getattr(self, "writer", None) is not None:
             self.writer.close()
-    
+        
+        # clean logs
+        import re
+        with self.log_path.open(mode="r+") as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                lines[i] = re.sub(r"..\d\dm", "", lines[i])
+            f.seek(0)
+            f.write("".join(lines[:-1]))
+
     def train(self, task_list: List[Tuple[str]], n_epcoh: int = 70, early_stop: int = 20):
+        self.logger.info(f"n_epoch: {n_epcoh}")
+        self.logger.info(f"early_stop: {early_stop}")
+        self.logger.info(f"task_list: {task_list}")
+
         # loaders
         train_loader = data.DataLoader(
             self.train_set, batch_size=128, shuffle=True, num_workers=self.num_worker, pin_memory=True
@@ -78,15 +113,11 @@ class CLTrainer:
             self.val_set, batch_size=128, shuffle=False, num_workers=self.num_worker, pin_memory=True
         )
 
-        # evaluator
-        cl_evaluator = ContinualLearningEvaluator(num_task=len(task_list))
-
         last_best: Union[iCaLRNet, None] = None
-
 
         e_digit = len(str(n_epcoh))
         ee_digit = len(str(early_stop))
-        t_digit = len(len(task_list))
+        t_digit = len(str(len(task_list)))
         torch.autograd.set_detect_anomaly(True)
 
         # == Pytorch Code Implementation of "Algorithm 2: iCaRL INCREMENTALTRAIN"
@@ -95,7 +126,7 @@ class CLTrainer:
             y: torch.Tensor
             loss: torch.Tensor
 
-            print(f"{Fore.BLUE}New Task: {task}")
+            self.logger.info(f"{Fore.BLUE}New Task: {task}")
             
             self.net.set_task(task=task)
             self.train_set.set_task(task=task)
@@ -162,20 +193,21 @@ class CLTrainer:
                 if top1_current_acc > max_top1_acc:
                     max_top1_acc = top1_current_acc
                     early_stop_cnt = 0
-                    last_best = copy.deepcopy(self.net)
-                    print(f"{Fore.GREEN}Task: [{task_idx:>t_digit}|{task}], Epoch: [{epoch:>e_digit}/{n_epcoh}], new top1 Acc: {top1_current_acc:>.5f}")
+                    last_best = copy.deepcopy(self.net.state_dict())
+                    self.logger.info(f"{Fore.GREEN}Task: [{task_idx:>{t_digit}}|{task}], Epoch: [{epoch:>{e_digit}}/{n_epcoh}], new top1 Acc: {top1_current_acc:>.5f}")
                     if not self.dry_run:
                         torch.save(self.net, (f:=self.checkpoint_path / f"task_{task_idx}-best.pt"))
-                        print("Save checkpoint!")
+                        self.logger.info("Save checkpoint!")
                 else:
                     early_stop_cnt += 1
-                    print(f"{Fore.GREEN}Task: [{task_idx:t_digit}|{task}], Epoch: [{epoch:>e_digit}/{n_epcoh}], Acc: [{top1_current_acc:>.5f}|{max_top1_acc:>.5f}], Early Stop: [{early_stop_cnt:>ee_digit}|{early_stop}]")
+                    self.logger.info(f"{Fore.GREEN}Task: [{task_idx:>{t_digit}}|{task}], Epoch: [{epoch:>{e_digit}}/{n_epcoh}], Acc: [{top1_current_acc:>.5f}|{max_top1_acc:>.5f}], Early Stop: [{early_stop_cnt:>{ee_digit}}|{early_stop}]")
 
                 if early_stop_cnt >= early_stop:
                     break
 
             # load best
-            self.net = last_best
+            self.net.load_state_dict(last_best)
+            self.logger.info(f"{Fore.GREEN}Task: [{task_idx:>{t_digit}}|{task}], end at Epoch: [{epoch:>{e_digit}}/{n_epcoh}], switch to best model, best Acc: [{max_top1_acc:>.5f}]")
 
             # == construct examplar set, Algorithm 4 iCaRL CONSTRUCTEXEMPLARSET
             self.train_set.examplar_set.construct_examplar_set(phi=self.net)
@@ -185,8 +217,7 @@ class CLTrainer:
             
 
             # test cl performance
-            all_num = 0
-            acc_num = 0
+            task_acc = []
             for past_task_idx, past_task in enumerate(task_list[:task_idx+1]):
                 p_all_num = 0
                 p_acc_num = 0
@@ -200,12 +231,10 @@ class CLTrainer:
                         # log
                         p_all_num += len(y)
                         p_acc_num += sum(y[:, 0] == y_classify[:, 0])
-                print(f"{Fore.BLUE}Past Task: [{past_task_idx:>task_idx}|{task}], Acc: {p_acc_num / p_all_num:>.5f}")
-
-                all_num += p_all_num
-                acc_num += p_acc_num
-            top1_acc_task = acc_num / all_num
-            print(f"{Fore.CYAN}After Task: [{task_idx:>t_digit}|{task}], CL_Acc: {top1_acc_task:>.5f}")
+                task_acc.append(p_acc_num / p_acc_num)
+                self.logger.info(f"{Fore.YELLOW}Past Task: [{past_task_idx:>{task_idx}}|{past_task}], Acc: {task_acc[-1]:>.5f}")
+            acc = sum(task_acc) / len(task_acc)
+            self.logger.info(f"{Fore.MAGENTA}After Task: [{task_idx:>{t_digit}}|{task}], CL_Acc: {acc:>.5f}")
 
         return self.net
 
@@ -214,7 +243,6 @@ class CLTrainer:
 if __name__ == "__main__":
     import pprint
 
-    # Fixme: if_shuffle 设为True则会由于其他脚本已经有了cifar100_label, cifar100_label2num, cifar100_num2label而导致出错
     if_shuffle = True
     num_task = 2
 
@@ -225,11 +253,10 @@ if __name__ == "__main__":
 
     for i in range(0, len(cifar100_labels.cifar100_labels), num_task):
         task_list.append(tuple(cifar100_labels.cifar100_labels[i: i+num_task]))
-    pprint.pprint(task_list)
 
     e = ExamplarSet()
     net = iCaLRNet(num_class=100, target_dataset="Cifar100", examplar_set=e)
-    net = CLTrainer(network=net, dry_run=True).train(task_list=task_list, n_epcoh=70, early_stop=35)
+    net = CLTrainer(network=net, dry_run=True, log=True).train(task_list=task_list, n_epcoh=70, early_stop=5)
 
     # debug, record the GPU memory use
     # from pytorch_memlab import LineProfiler
